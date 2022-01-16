c42927bfba9bd4740f3cafeb30f235a180f50a67
PaulAchard <58505638+PaulAchard@users.noreply.github.com>
2022-01-16 19:47:19 +0100

    test de linear

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 # ne plus être limité lors des prints
-np.set_printoptions(threshold=sys.maxsize)
+# np.set_printoptions(threshold=sys.maxsize)
 
 digits = datasets.load_digits()
 x = digits.images.reshape((len(digits.images), -1))
 y = digits.target
 
+# séparation du dataset en données "d'apprentissage" et de test
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.50, random_state=0)
 
 model = LogisticRegression(verbose=True, max_iter=4000, solver='lbfgs')
